d8d8c675f03d721e7e500b11180cdf698e121ea3
RianAsmara <gagahkalem@gmail.com>
2021-05-30 20:54:24 +0800

    Update categoriesTab.dart

--- lib/screen/atoms/categoriesTab.dart
+++ lib/screen/atoms/categoriesTab.dart
@@ -10,41 +10,37 @@ class CategoriesTab extends StatelessWidget {
         margin: EdgeInsets.only(left: 14.4, top: 28.8),
         child: DefaultTabController(
           length: 4,
-          child: Tabs(
+          child: TabBar(
               labelPadding: EdgeInsets.only(left: 14.4, right: 14.4),
               indicatorPadding: EdgeInsets.only(left: 14.4, right: 14.4),
               isScrollable: true,
-              labelColor: Color(0xff000000),
-              unselectedLabelColor: Color(0xff8a8a8a),
-              labelStyle: GoogleFonts.lato(
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
-              ),
-              unselectedLabelStyle: GoogleFonts.lato(
-                fontSize: 14,
-                fontWeight: FontWeight.w700,
-              ),
+              labelColor: Color(0xFF000000),
+              unselectedLabelColor: Color(0xFF8a8a8a),
+              labelStyle:
+                  GoogleFonts.lato(fontSize: 14, fontWeight: FontWeight.w700),
+              unselectedLabelStyle:
+                  GoogleFonts.lato(fontSize: 14, fontWeight: FontWeight.w700),
               indicator: RoundedRectangleTabIndicator(
-                  color: Color(0xff000000), weight: 2.4, width: 14.4),
+                  color: Color(0xFF000000), weight: 2.4, width: 14.4),
               tabs: [
                 Tab(
                   child: Container(
-                    child: Text("Recommended"),
+                    child: Text('Recommended'),
                   ),
                 ),
                 Tab(
                   child: Container(
-                    child: Text("Popular"),
+                    child: Text('Popular'),
                   ),
                 ),
                 Tab(
                   child: Container(
-                    child: Text("New Destination"),
+                    child: Text('New Destination'),
                   ),
                 ),
                 Tab(
                   child: Container(
-                    child: Text("Hidden Paradise"),
+                    child: Text('Hidden Gems'),
                   ),
                 )
               ]),
